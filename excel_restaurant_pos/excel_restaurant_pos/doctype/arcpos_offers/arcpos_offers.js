afe88ec582495a02fa86c5c01ef5de0efefa912d
dev-Anamul <ah9737677@gmail.com>
2026-03-28 11:37:11 +0000

    feat: added new doctype called arcpos offers

---
+++ excel_restaurant_pos/excel_restaurant_pos/doctype/arcpos_offers/arcpos_offers.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Sohanur Rahman and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('ArcPOS Offers', {
+	// refresh: function(frm) {
+
+	// }
+});
