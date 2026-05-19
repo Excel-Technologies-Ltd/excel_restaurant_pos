9ac50533c7872e4aa36af279a4cb7d5bea91409c
dev-Anamul <ah9737677@gmail.com>
2026-05-19 10:25:33 +0000

    fix: wrong report patches issues

---
+++ excel_restaurant_pos/patches/v1_5_0/update_get_net_sales_by_service_procedure.py
@@ -0,0 +1,34 @@
+import frappe
+
+
+def execute():
+    frappe.db.sql("DROP PROCEDURE IF EXISTS `GetNetSalesByServiceType`")
+
+    sql_query = """
+    CREATE PROCEDURE `GetNetSalesByServiceType`(
+        IN p_from_date DATE,
+        IN p_to_date DATE
+    )
+    BEGIN
+        SELECT
+            custom_order_from,
+            custom_service_type,
+            COUNT(`name`) AS OrderCount,
+            SUM(COALESCE(net_total, 0)) AS NetSales,
+            ROUND(
+                SUM(COALESCE(net_total, 0))
+                / SUM(SUM(COALESCE(net_total, 0))) OVER () * 100,
+                2
+            ) AS Percentage
+        FROM
+            `tabSales Invoice`
+        WHERE
+            docstatus = 1
+            AND posting_date >= p_from_date
+            AND posting_date <= p_to_date
+        GROUP BY
+            custom_order_from,
+            custom_service_type;
+    END
+    """
+    frappe.db.sql(sql_query)
