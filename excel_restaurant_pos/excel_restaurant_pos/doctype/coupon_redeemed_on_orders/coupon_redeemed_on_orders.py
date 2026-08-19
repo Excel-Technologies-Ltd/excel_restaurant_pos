2758f630ef4b74ce43550130cdf9c4d36e0f677a
dev-Anamul <ah9737677@gmail.com>
2026-08-19 08:55:18 +0000

    feat: gift card customization

---
+++ excel_restaurant_pos/excel_restaurant_pos/doctype/coupon_redeemed_on_orders/coupon_redeemed_on_orders.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2026, Sohanur Rahman and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class CouponRedeemedonOrders(Document):
+	pass
